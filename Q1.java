971e3215de1325fb11da7863f12748c16e1bd747
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-03 18:40:58 -0400

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,10 +1,22 @@
-import java.util.*;
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+import java.util.Scanner;
+
+/**
+ * This program checks if a word is a palindrome or not.  if it reads the same backward as forward. 
+ * A few examples are “civic”, “radar” and “level”.
+ * @author Uzair Ahmed 
+ */
 public class Q1 {
   public static void main(String[] args) {
-
         
       String forward;
-	  String backward = "";
+      String backward = "";
 	  
       Scanner in = new Scanner(System.in);   
       System.out.println("Please enter a word/palindrome");  
@@ -13,12 +25,12 @@ public class Q1 {
       forward2=forward2.replaceAll("[^a-z]","");
       int length = forward.length();   
       
-	  for ( int i = length - 1; i >= 0; i-- )  
-         backward = backward + forward.charAt(i);  
+      for ( int i = length - 1; i >= 0; i-- )  
+      backward = backward + forward.charAt(i);  
       if (forward.equals(backward))  
-         System.out.println("It is a palindrome.");  
+      System.out.println("It is a palindrome.");  
       else  
-         System.out.println("It isn't a palindrome."); 
+      System.out.println("It isn't a palindrome."); 
 
     }
 }
