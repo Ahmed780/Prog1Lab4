ac73f15bcc6d0d2ee9bcac2206e92e8b424209fd
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-03 19:03:50 -0400

    Update Q44.java

--- Q44.java
+++ Q44.java
@@ -1,24 +1,37 @@
-import java.util.*;
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+import java.util.Scanner;
+
+/**
+ * This programs asks the user for a word.
+ * If the word does not contain an @ character, then the user is asked for another word. 
+ * @author Uzair Ahmed 
+ */
 
 public class Word {
   public static void main(String[] args) {
 
     Scanner sc = new Scanner (System.in);
-        String word;
+    String word;
         
-        System.out.println("please enter a word with @");
-        word = sc.next();
+    System.out.println("please enter a word with @");
+    word = sc.next();
         
-        while (! word.contains("@")) {
-		//while the word doesnt contain @, ask the user for another word (@)
-        System.out.println("Try again");
-        word = sc.next();
-		System.out.println("The word is "+word);
-        }
+    while (! word.contains("@")) {
+    //while the word doesnt contain @, ask the user for another word (@)
+    System.out.println("Try again");
+    word = sc.next();
+    System.out.println("The word is "+word);
+    }
 
 
     }
 }
   
 
-//Write a program that expects a word containing the @ character as an input. If the word does not contain an @ character, then your program should keep prompting the user for a word. When the user types in a word containing an @ character, the program should simply print the word and terminate
+
