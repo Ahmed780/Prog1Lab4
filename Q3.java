b9baf5704f73d0796eb4305e759d1c5e9827ed26
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-03 18:58:19 -0400

    Update Q3.java

--- Q3.java
+++ Q3.java
@@ -1,6 +1,18 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
 
-import java.util.*;
+import java.util.Scanner;
 
+/**
+ * This program  generates a random number between 0 and 100 and asks the user to guess it.  
+ * If the number is guessed, the user will be asked if they want to play again
+ * if the guess is incorrect and 10 trials are used , the game will end. 
+ * @author Uzair Ahmed 
+ */
 public class Q3 {
 	public static void main(String[] args) {
 
@@ -8,26 +20,25 @@ public class Q3 {
 		double word;
 
 		System.out.println(random);
-
 		Scanner sc = new Scanner(System.in);
-
 		System.out.println("Guess the number");
 		word = sc.nextDouble();
 		
 		int a = 1;
 		for (int i = 1; i < 10; i++) {
 			
-			if (word == random) {
-				System.out.println("\nCorrect");
-				i = 10;
-			} else {
-				System.out.println("Try Again!");
-				word = sc.nextDouble();
-				a += 1;
-			}
+		if (word == random) {
+		System.out.println("\nCorrect");
+		i = 10;
+		} 
+		else {
+		System.out.println("Try Again!");
+		word = sc.nextDouble();
+		a += 1;
+		}
 		}
 		if(a >= 10) {
-			System.out.println("\nUnlucky");			
+		System.out.println("\nUnlucky");			
 		}
 	}
 
