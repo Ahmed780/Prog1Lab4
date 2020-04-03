ce80847189b20cc41bb8dfa6ae5e003e7ce0c9ac
Ahmed780 <60394060+Ahmed780@users.noreply.github.com>
2020-04-03 19:06:04 -0400

    Update Q48.java

--- Q48.java
+++ Q48.java
@@ -1,5 +1,16 @@
-import java.util.*;
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
 
+import java.util.Scanner;
+
+/**
+ * This program that uses a for loop to count how many multiples of 7 are between 33 and 97, inclusive
+ * @author Uzair Ahmed 
+ */
 public class Count {
   public static void main(String[] args) {
 
@@ -9,11 +20,9 @@ public class Count {
         if(i%7==0)	{
         System.out.println(i);
         multiples+=1;
-            }
+        }
         }
         System.out.println("Number of multiples of 7 between 33 and 97: "+multiples);
   }
 }
   
-
-// Write a program that uses a for loop to count how many multiples of 7 are between 33 and 97, inclusive
